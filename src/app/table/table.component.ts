688a92930643933fdae713ee384267303bdc87ea
natsuhiko KATO <kato@natsuhiko.jp>
2018-11-13 11:33:11 +0700

    Add: functions for display table cell

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -9,4 +9,20 @@ import Tweet from '../tweet';
 
 export class TableComponent {
   @Input() tweets: Tweet[];
+  displayTweets(tweet: string) {
+    if (tweet.length < 50) return tweet;
+    return `${tweet.substr(0,(50))}...`
+  }
+  displayCount(count: number) {
+    if (count === 0) return '-';
+    return count;
+  }
+  displayTags(tags: string[]) {
+    if (tags.length <= 2) return tags;
+    return tags.slice(0, 1);
+  }
+  displayDate(dateTime: string) {
+    const dateSplited = dateTime.split(' - ')[1].split(' ');
+    return `${dateSplited[1]} ${dateSplited[0]}, ${dateSplited[2]}`;
+  }
 }
